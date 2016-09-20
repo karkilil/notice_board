6fb3fabe2c4d84c88edec9c2b67b711bb49dba72
karkilil <lilkarki134@gmail.com>
2016-09-20 12:28:45 +0545

    add controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Notice\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Notice\Http\Requests;
+
+class AdminController extends Controller
+{
+    public function login(){
+        
+    }
+}
