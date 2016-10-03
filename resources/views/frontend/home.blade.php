0ac4039fc74bc3ac32241f2771d150fc194218bc
NISCHAL <emarkib@gmail.com>
2016-10-03 23:52:52 +0545

    home design part

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,9 +1,8 @@
 @extends('frontend.layout')
 @section('content')
+    <div class="col-md-6" >
 
-    <div class="col-md-6" style="padding: 0; margin-bottom: 20px;">
-
-        <div class="container" style="margin-top: 60px;">
+     {{--   <div class="container" style="margin-top: 60px;">
             <h3 style="color: #1b6d85;">View Your Notice Here</h3>
             <!-- Trigger the modal with a button -->
             <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Open Notice</button>
@@ -30,13 +29,28 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>--}}
+
+
+
+            <div class="card-content  well">
+                <p> Notice Board System deals with publishing
+                    of the notice and forwarding of the notice
+                    to the students. It is the web based system
+                    which is easy to access to all the users.
+                    Regardless of the location and type of
+                    machine of system, user can access system.
+                </p>
+            </div>
+
+
+
     </div>
 
-    <div class="col-md-6" style="padding: 0; margin-bottom: 20px;">
-        <div class="panel panel-primary" style="margin-top: 60px;">
+    <div class="col-md-6" >
+        {{--<div class="panel panel-primary" style="margin-top: 60px;">
             <div class="panel-heading">
-                <h3 class="panel-title">Notice Board System Log In Panel </h3>
+                <h3 class="panel-title">Log In Panel </h3>
             </div>
 
             <div class="panel-body">
@@ -72,10 +86,58 @@
                 </form>
             </div>
         </div>
+--}}
+
+
+        <div class="card">
+            <div class="card-block" style="padding-left: 50px;">
+                <div class="row col-sm-12">
+                    <form method="post" action="#">
+                        <div class="form-group">
+                            <div class="input-group ">
+                        <span class="input-group-addon">
+							<i class="glyphicon glyphicon-user"></i>
+						</span>
+                                <input name="username" type="text" class="form-control" placeholder="Username">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+						<span class="input-group-addon">
+							<i class="glyphicon glyphicon-lock"></i>
+						</span>
+                                <input name="password" type="password" class="form-control" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input type="checkbox" data-group-cls="btn-group-lg">
+                                <span>Remember me</span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                            <button class="btn waves-effect waves-light blue" type="submit" name="login">
+                                <i class="material-icons right">Login</i>
+                            </button>
+
+                                </div>
+                        </div>
+                            <span><a href="#" >Forget Password?</a></span>
+
+                    </form>
+
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="btn waves-effect waves-light blue"><a href="register">Register New User</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
-   <div class="footer">
-       <p style="float:left; margin-left: 10px; margin-right: 20px; margin-top: 200px; color: #1b6d85;">Copyright © Notice Board System</p>
-       <p style="float:left; margin-top: 200px; color: #1b6d85;">This Web application is design and developed by Students of National College of Computer Studies B.Sc.CSIT 8th Semsester</p>
-   </div>
+
 
     @endsection
--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -7,28 +7,52 @@
     <title>@yield('title')</title>
     <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- Material Design Bootstrap -->
+    <link href="/css/mdb.min.css" rel="stylesheet">
+    <link href="/css/app.css" type="text/css" rel="stylesheet" media="screen" />
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
     <!-- custom css -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="dist/css/bootstrap-material-design.min.css" type="text/css" rel="stylesheet" media="screen" />
+    <link href="dist/css/ripples.min.css" type="text/css" rel="stylesheet" media="screen" />
     {{--font-awesome--}}
     {{--<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">--}}
     {{--materialize--}}
-    {{--<link rel="stylesheet" href="/materialize/css/materialize.min.css">--}}
+
+
+    @yield('styles')
 </head>
 <body>
-<img class="img" src="img/noticeBoard.png" alt="rect">
+{{--<img class="img" src="img/noticeBoard.png" alt="rect">--}}
+        <!-- our markup -->
 
+
+<!-- an image for demonstration purposes -->
+{{--
+<img src="img/noticeBoard.png" width="782" height="100" alt="Big Image"/>
+--}}
+
+@include('frontend.header');
 <div class="container">
 
-    @yield('content')<!-- /container -->
+
+        @yield('content')<!-- /container -->
 
 </div>
 
+
+@include('frontend.footer')
 <!-- jQuery -->
 <script src="/js/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="/js/bootstrap.min.js"></script>
-{{--materialize--}}
-{{--<script src="/materialize/js/materialize.min.js"></script>--}}
-
+{{--<script src="/js/bootstrap.min.js"></script>--}}
+<script type="text/javascript" src="dist/js/material.min.js" />
+<script type="text/javascript" src="dist/js/ripples.min.js" />
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="/js/mdb.min.js"></script>
+@yield('script')
+{{--<script type="text/javascript">
+    $.material.init()
+</script>--}}
 </body>
 </html>
