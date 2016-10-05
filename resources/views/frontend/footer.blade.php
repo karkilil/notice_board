9f00e7f3a9f7942bf7b5366f6332a5225d4fe08d
NISCHAL <emarkib@gmail.com>
2016-10-05 08:30:43 +0545

    home design part with footer header

---
+++ resources/views/frontend/footer.blade.php
@@ -0,0 +1,5 @@
+
+<div class="well">
+    <p class="embed-responsive-item">Copyright © Notice Board System</p>
+    <p class="embed-responsive-item">This Web application is design and developed by Students of National College of Computer Studies B.Sc.CSIT 8th Semester</p>
+</div>
---
+++ resources/views/frontend/header.blade.php
@@ -0,0 +1,10 @@
+{{--<div class="page-header" style="padding-left: 50px;">  <!-- page header created using .page-header -->
+
+    <h1>Online Notice Board System <small> ONBS</small></h1>
+    <p class="lead">+ Easy Notice</p>
+
+</div>--}}
+<header>
+
+    <h1 class="h1-responsive">Online Notice Board System</h1>
+   </header>
